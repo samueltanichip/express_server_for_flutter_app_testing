c3c8ebf077aeeb82f3fa093abf6cbf47abf5b5ab
samueltanichip <samueltani@chiptronic.com.br>
2025-03-25 10:13:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,50 @@
-const express = require('express')//
-const app = express()
-//const bodyParser = require('body-parser');
-//app.use(bodyParser);
-// create application/json parser
-var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
- 
-// create application/x-www-form-urlencoded parser
-//var urlencodedParser = bodyParser.urlencoded({ extended: false })
-const port = 3000
-const auth = (req,res,  next) => {
-	const headers = req.headers;
-	if (headers['x-dsn']!='abcd-abcd')
-	{
-		res.send('Auth Failed');
-		return;
-	}
-	next();
-}
-app.get('/',  (req, res) => {
-	res.send('testeeee');
-  })
-app.get('/testgetrequest',  (req, res) => {
-	res.send('Hello World!');
-  })
-app.get('/testwithheader', auth,  (req, res) => {
-  res.send('Hello World! with header');
-})
-app.post('/testwithheader', auth ,jsonParser,  (req, res) => {
-	//console.log(req.body);
-	res.send('Hello World! with header n body'+req.body.name);
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const express = require('express');
+const bodyParser = require('body-parser');
 
-//express started
+// Criação da aplicação Express
+const app = express();
+
+// Middlewares
+app.use(bodyParser.json()); // Substitui seu jsonParser individual
+app.use(bodyParser.urlencoded({ extended: true })); // Para formulários HTML
+
+// Middleware de autenticação
+const auth = (req, res, next) => {
+  const headers = req.headers;
+  if (headers['x-dsn'] !== 'abcd-abcd') {
+    return res.status(401).send('Auth Failed');
+  }
+  next();
+};
+
+// Rotas
+app.get('/', (req, res) => {
+  res.send('testeeee');
+});
+
+app.get('/testgetrequest', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/testwithheader', auth, (req, res) => {
+  res.send('Hello World! with header');
+});
+
+app.post('/testwithheader', auth, (req, res) => {
+  // Agora o bodyParser.json() está aplicado globalmente
+  res.send(`Hello World! with header n body ${req.body.name}`);
+});
+
+// Exportação para testes
+module.exports = app;
+
+// Inicialização do servidor apenas quando não for teste
+if (process.env.NODE_ENV !== 'test') {
+  const port = process.env.PORT || 3000;
+  const server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+  
+  // Para possível desligamento limpo
+  module.exports.server = server;
+}

32f09f87e6094557c94074565bc8295c227aa5af
samueltanichip <samueltani@chiptronic.com.br>
2025-04-22 09:18:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/testgetrequest', (req, res) => {
 });
 
 app.get('/testwithheader', auth, (req, res) => {
-  res.send('teste polling');
+  res.send('teste');
 });
 
 app.post('/testwithheader', auth, (req, res) => {
